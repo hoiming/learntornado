83d3fe3f63b57fd2a2fcfc3b39fd0139019a4eb7
haiming <haiming_liang@yahoo.com>
2013-12-20 14:57:24 +0800

    chapter 5 async web service

--- burtbook/main.py
+++ burtbook/main.py
@@ -3,21 +3,28 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import os.path
+import pdb
 
+import pymongo
 from tornado.options import define, options
 define("port", default=8000, help = "run on the given port",type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-                (r"/", RecommendedHandler),
+                (r"/", MainHandler),
+                (r"/recommended/", RecommendedHandler),
+                (r"/edit/([0-9Xx\-]+)", BookEditHandler),
+                (r"/add/",BookEditHandler),
                 ]
         settings = dict(
                 template_path=os.path.join(os.path.dirname(__file__),"templates"),
                 static_path=os.path.join(os.path.dirname(__file__), "static"),
                 ui_modules={'Hello': HelloModule,'Book':BookModule},
                 )
-        super(Application, self).__init__(handlers, **settings)
+        conn = pymongo.Connection("localhost", 27017)
+        self.db = conn["bookstore"]
+        super(Application, self).__init__(handlers,debut=True, **settings)
 
 class BookModule(tornado.web.UIModule):
     def render(self, book):
@@ -35,24 +42,54 @@ class MainHandler(tornado.web.RequestHandler):
                 )
 class RecommendedHandler(tornado.web.RequestHandler):
     def get(self):
+        coll = self.application.db.books
+        books = coll.find()
+        if not books:
+            books=dict()
         self.render(
                 "recommended.html",
                 page_title="Burt's Books | Recommended Reading",
                 header_text="Recommended Reading",
-                books=[
-                    {
-                        "title":"Programming collective Intelligence",
-                        "subtitle":"Building Smart Web 2.0 Applications",
-                        "image":"/static/images/collective_intelligence.gif",
-                        "author":"Toby Segaran",
-                        "date_added":1310248056,
-                        "date_released":"August 2007",
-                        "isbn":"978-0-596-52932-1",
-                        "description":"<p>This is lahblah.</p>"
- }
- ]
+                books= books
                 )
 
+class BookEditHandler(tornado.web.RequestHandler):
+    def get(self, isbn=None):
+        book = dict()
+        if isbn:
+            coll = self.application.db.books
+            book = coll.find_one({"isbn":isbn})
+        if book:
+            self.render("book_edit.html",
+                    page_title="Burt's Books",
+                    header_text="Edit book",
+                    book=book)
+        else:
+            self.render("book_edit.html",
+                    page_title="Burt's Books",
+                    header_text="Edit book",
+                    book=dict())
+    def post(self, isbn=None):
+        import time
+        pdb.set_trace()
+        book_fields = ['isbn', 'title', 'subtitle', 'image', 'author',\
+                'date_released', 'description']
+        coll = self.application.db.books
+        book = dict()       
+        if isbn:
+            book = coll.find_one({"isbn":isbn}) 
+        if not  book:
+            book = dict()
+
+        for key in book_fields:
+            book[key] = self.get_argument(key, None)
+        if isbn:
+            book['date_added'] = int(time.time())
+            coll.save(book)
+        else:
+            book['date_added'] = int(time.time())
+            coll.insert(book)
+        self.redirect("/recommended/")
            
 if __name__ == "__main__":
     tornado.options.parse_command_line()
